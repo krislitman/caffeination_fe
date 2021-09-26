986d397451aaeb94cc547bfd1ff0379ffddc35fd
Kris Litman <kris.d.litman@gmail.com>
2021-09-26 16:00:27 -0400

    Added hotwire to Gemfile

--- spec/feature/user_registration_spec.rb
+++ spec/feature/user_registration_spec.rb
@@ -14,4 +14,21 @@ RSpec.describe 'As a visitor', type: :feature do
       expect(page).to have_field(:query)
     end
   end
+  scenario 'When I click on ~Sign up~, I am brought to a page to enter more information' do
+    visit root_path
+    click_button 'Sign up'
+
+    expect(current_path).to eq(sign_up_index_path)
+    within '#sign_up_form' do
+      expect(page).to have_field(:first_name)
+      expect(page).to have_field(:last_name)
+      expect(page).to have_field(:address_1)
+      expect(page).to have_field(:address_2)
+      expect(page).to have_field(:city)
+      expect(page).to have_field(:state)
+      expect(page).to have_field(:zipcode)
+      expect(page).to have_field(:email)
+      expect(page).to have_field(:favorite_beverage)
+    end
+  end
 end
