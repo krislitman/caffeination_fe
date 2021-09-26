925039450bf8ff0e1591d33ac57006fab90cfe85
Kris Litman <kris.d.litman@gmail.com>
2021-09-26 00:02:31 -0400

    First test passing, buttons and first routes for user sign up and log in have been created. Same with the form for zipcode searching

--- spec/feature/user_registration_spec.rb
+++ spec/feature/user_registration_spec.rb
@@ -5,11 +5,13 @@ RSpec.describe 'As a visitor', type: :feature do
     visit root_path
 
     expect(page).to have_content('Caffeination')
-    expect(page).to have_button('Sign up')
-    expect(page).to have_button('Log in')
+    within '#user_options' do
+      expect(page).to have_button('Sign up')
+      expect(page).to have_button('Log in')
+    end
     within '#zipcode_search' do
       expect(page).to have_content('Where would you like to grab coffee?')
-      expect(page).to have_field('zipcode')
+      expect(page).to have_field(:query)
     end
   end
 end
