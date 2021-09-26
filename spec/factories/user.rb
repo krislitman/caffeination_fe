352f5df9c613c305ad322b2cc6abc52aec663239
Kris Litman <kris.d.litman@gmail.com>
2021-09-26 17:30:19 -0400

    Added user to models, building out unit test for user

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,9 +1,18 @@
 FactoryBot.define do
   factory :user do
-    sequence :first_name do |n|
-      "person #{n}"
+    sequence :username do |n|
+      "user-#{n}"
     end
-    last_name  { "Doe" }
-    admin { false }
+    first_name {"#{Faker::Games::Pokemon.name}"}
+    last_name {"#{Faker::Games::Pokemon.move}"}
+    address_1 {"#{Faker::Games::Pokemon.location}"}
+    sequence :address_2 do |n|
+      "Apt -#{n}"
+    end
+    city {"#{Faker::Address.city}"}
+    state {"#{Faker::Address.state_abbr}"}
+    zipcode {"#{Faker::Address.zip_code}"}
+    email {"#{Faker::Internet.safe_email}"}
+    favorite_beverage {"#{Faker::Coffee.blend_name}"}
   end
 end
--- spec/unit/user_spec.rb
+++ spec/unit/user_spec.rb
@@ -1,7 +1,11 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  describe "Validations" do
-    it {should validate_presence_of :first_name}
+  context "Validations" do
+    before { FactoryBot.build(:user) }
+
+    it do
+      should validate_presence_of :first_name
+    end
   end
 end
