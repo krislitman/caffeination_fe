bb2ca1b3ea61863d54cb2273a651a3bf0c0c600c
Kris Litman <kris.d.litman@gmail.com>
2021-09-29 23:29:19 -0400

    Created modular setup for creating different objects via API calls to the BE repo

--- app/services/creation_service.rb
+++ app/services/creation_service.rb
@@ -1,13 +1,27 @@
 class CreationService
   class << self
     def store_user_and_session(user, session)
-
+      type = {
+        store_user_and_session: true
+      }
+      local_connection(
+        user: user,
+        session: session,
+        type: type
+      )
     end
-  end
 
-  private
-
-  def local_connection
-    # will need to make API call here
+    def local_connection(**args)
+      case
+      when args[:type] == :store_user_and_session
+        HTTParty.post(
+          'http://localhost:3001',
+          params: {
+            user: args[:user],
+            session: args[:session]
+          }
+        )
+      end
+    end
   end
 end
