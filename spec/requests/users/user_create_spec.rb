e9932e7824626adc561f96ecf7182dd46479ad9b
Kris Litman <kris.d.litman@gmail.com>
2021-09-29 22:50:43 -0400

    Creating logic to also store information in the back end

---
+++ spec/requests/users/user_create_spec.rb
@@ -0,0 +1,11 @@
+require "rails_helper"
+
+RSpec.describe "When a User is created on the FE", type: :request do
+  context "The user and session data are stored in the BE" do
+    @user = FactoryBot.create(:user)
+    session = {user_id: @user.id}
+    response = CreationService.store_user_and_session(@user, session)
+
+    expect(response).to be_a(Hash)
+  end
+end
