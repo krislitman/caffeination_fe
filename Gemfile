accf31a61b51de741d4f7fe2a211be094717edb6
Kris Litman <kris.d.litman@gmail.com>
2021-11-13 19:06:44 -0500

    Added Figaro gem, and first test for Yelp API call

--- Gemfile
+++ Gemfile
@@ -23,6 +23,9 @@ gem 'bcrypt', '~> 3.1.7'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
+# Figaro for key storage
+gem 'figaro'
+
 # API Calls
 gem 'httparty'
 
---
+++ spec/requests/yelp/yelp_search_tampa_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe "Yelp API call initial test" do
+  describe "A guest can enter a zipcode into form" do
+    it "Can search for local coffee shops" do
+      visit home_path
+      fill_in :query, with: "33602"
+      click_button "Search"
+
+      expect(current_path).to eq(shops_path)
+      expect(page).to have_content("Coffee Shops near 33602")
+    end
+  end
+end
