e002eb7db4d9be9c66dae3e11c691d081eca14d0
Kris Litman <kris.d.litman@gmail.com>
2021-11-21 17:17:22 -0500

    Bootstrap install and working, coffee icon button added with Font Awesome gem

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,14 @@
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+require("stylesheets/application.scss")
+require("@fortawesome/fontawesome-free/js/all")
 
 Rails.start()
 ActiveStorage.start()
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
