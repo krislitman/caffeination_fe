b9caeb2f0d11df88ac933e538fdf9e24a17629d3
Kris Litman <kris.d.litman@gmail.com>
2022-01-08 22:59:16 -0500

    First test passing, capybara button element found on homepage by xpath

--- spec/feature/user/current_location/user_can_find_current_location_spec.rb
+++ spec/feature/user/current_location/user_can_find_current_location_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe 'As a guest', type: :feature do
   scenario 'When I visit the home page, I see a button to find my current location' do
     visit root_path
 
-    expect(page).to have_button('Find My Location')
+    button = find(:xpath, "/html/body/div[1]/main/div[2]/form/button")
+
+    expect(button).to be_an_instance_of(Capybara::Node::Element)
   end
 end
