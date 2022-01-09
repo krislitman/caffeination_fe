a8e9496f5d71cd015fb8b088253aa9c5314946c3
Kris Litman <kris.d.litman@gmail.com>
2022-01-08 23:43:54 -0500

    Added VCR to current_location_spec

--- spec/requests/current_location/user_can_find_current_location_spec.rb
+++ spec/requests/current_location/user_can_find_current_location_spec.rb
@@ -10,15 +10,17 @@ RSpec.describe 'As a guest', type: :feature do
   end
 
   scenario 'When I click button to find current location, I am redirected back to the home page with postal code entered' do
-    visit root_path
+    VCR.use_cassette('requests/current_location', match_requests_on: %i[body]) do
+      visit root_path
 
-    find(:xpath, "/html/body/div[1]/main/div[2]/form/button").click
+      find(:xpath, "/html/body/div[1]/main/div[2]/form/button").click
 
-    test_location = CurrentLocationService.get_current_location
+      test_location = CurrentLocationService.get_current_location
 
-    expect(current_path).to eq(root_path)
-    within("#zipcode_search") do
-      expect(page).to have_content(test_location)
+      expect(current_path).to eq(root_path)
+      within("#zipcode_search") do
+        expect(page).to have_content(test_location)
+      end
     end
   end
 end
