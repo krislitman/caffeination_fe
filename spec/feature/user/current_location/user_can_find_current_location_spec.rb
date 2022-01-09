886b05c5725ab1061afa9cadbab48cabf91534e1
Kris Litman <kris.d.litman@gmail.com>
2022-01-08 23:04:47 -0500

    Added second spec for clicking the button, redirecting to home page and seeing postal code entered into search form

--- spec/feature/user/current_location/user_can_find_current_location_spec.rb
+++ spec/feature/user/current_location/user_can_find_current_location_spec.rb
@@ -8,4 +8,17 @@ RSpec.describe 'As a guest', type: :feature do
 
     expect(button).to be_an_instance_of(Capybara::Node::Element)
   end
+
+  scenario 'When I click button to find current location, I am redirected back to the home page with postal code entered' do
+    visit root_path
+
+    find(:xpath, "/html/body/div[1]/main/div[2]/form/button").click
+
+    test_location = CurrentLocationService.get_current_location
+
+    expect(current_path).to eq(root_path)
+    within("#zipcode_search") do
+      expect(page).to have_content(test_location)
+    end
+  end
 end
