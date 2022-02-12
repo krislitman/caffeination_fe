d65553ddb49d17505372b0d001329523d0f4eac4
Kris Litman <kris.d.litman@gmail.com>
2022-02-11 23:11:56 -0500

    Refactored current location controller

--- app/controllers/current_location_controller.rb
+++ app/controllers/current_location_controller.rb
@@ -1,11 +1,6 @@
 class CurrentLocationController < ApplicationController
   def find
-    location = CurrentLocationService.get_current_location
-
-    if location.nil?
-      # Add error handling
-    else
-      redirect_to root_path(current_location: location)
-    end
+    location = CurrentLocationService.get_current_location || nil
+    redirect_to root_path(current_location: location)
   end
 end
