5ccf24b3da405e6a88b4dbb8d94df9000c1d99d3
Kris Litman <kris.d.litman@gmail.com>
2022-04-30 20:18:18 -0400

    Adding middleware to initializers

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+	provider :google_oauth2, Figaro.env.GOOGLE_CLIENT_ID, Figaro.env.GOOGLE_CLIENT_SECRET
+  end
