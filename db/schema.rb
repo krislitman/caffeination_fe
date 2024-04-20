72d1a0726f42a57a95deb30482d21beea1c17443
Kris Litman <kris.d.litman@gmail.com>
2024-04-20 14:47:33 -0600

    Updating Rails to 7.0.0

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,97 +10,96 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_230_128_204_707) do
+ActiveRecord::Schema[7.0].define(version: 2024_04_20_204330) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'active_storage_attachments', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'record_type', null: false
-    t.bigint 'record_id', null: false
-    t.bigint 'blob_id', null: false
-    t.datetime 'created_at', null: false
-    t.index ['blob_id'], name: 'index_active_storage_attachments_on_blob_id'
-    t.index %w[record_type record_id name blob_id], name: 'index_active_storage_attachments_uniqueness',
-                                                    unique: true
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table 'active_storage_blobs', force: :cascade do |t|
-    t.string 'key', null: false
-    t.string 'filename', null: false
-    t.string 'content_type'
-    t.text 'metadata'
-    t.string 'service_name', null: false
-    t.bigint 'byte_size', null: false
-    t.string 'checksum', null: false
-    t.datetime 'created_at', null: false
-    t.index ['key'], name: 'index_active_storage_blobs_on_key', unique: true
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", precision: nil, null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table 'active_storage_variant_records', force: :cascade do |t|
-    t.bigint 'blob_id', null: false
-    t.string 'variation_digest', null: false
-    t.index %w[blob_id variation_digest], name: 'index_active_storage_variant_records_uniqueness', unique: true
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table 'addresses', force: :cascade do |t|
-    t.bigint 'user_id', null: false
-    t.string 'line1'
-    t.string 'line2'
-    t.string 'city'
-    t.string 'postal_code'
-    t.string 'state'
-    t.string 'country'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['user_id'], name: 'index_addresses_on_user_id'
+  create_table "addresses", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "line1"
+    t.string "line2"
+    t.string "city"
+    t.string "postal_code"
+    t.string "state"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_addresses_on_user_id"
   end
 
-  create_table 'coffee_shops', force: :cascade do |t|
-    t.string 'name', null: false
-    t.string 'image_url'
-    t.float 'rating'
-    t.jsonb 'coordinates', default: '{}', null: false
-    t.jsonb 'transaction_types', default: '{}', null: false
-    t.jsonb 'location', default: '{}', null: false
-    t.string 'phone'
-    t.string 'url'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'search_phrase'
-    t.string 'yelp_id'
-    t.string 'hours'
-    t.string 'price'
-    t.boolean 'open'
+  create_table "coffee_shops", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "image_url"
+    t.float "rating"
+    t.jsonb "coordinates", default: "{}", null: false
+    t.jsonb "transaction_types", default: "{}", null: false
+    t.jsonb "location", default: "{}", null: false
+    t.string "phone"
+    t.string "url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "search_phrase"
+    t.string "yelp_id"
+    t.string "hours"
+    t.string "price"
+    t.boolean "open"
   end
 
-  create_table 'coffee_shops_users', id: false, force: :cascade do |t|
-    t.bigint 'coffee_shop_id', null: false
-    t.bigint 'user_id', null: false
-    t.index ['coffee_shop_id'], name: 'index_coffee_shops_users_on_coffee_shop_id'
-    t.index ['user_id'], name: 'index_coffee_shops_users_on_user_id'
+  create_table "coffee_shops_users", id: false, force: :cascade do |t|
+    t.bigint "coffee_shop_id", null: false
+    t.bigint "user_id", null: false
+    t.index ["coffee_shop_id"], name: "index_coffee_shops_users_on_coffee_shop_id"
+    t.index ["user_id"], name: "index_coffee_shops_users_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'first_name', null: false
-    t.string 'last_name', null: false
-    t.string 'address_1'
-    t.string 'address_2'
-    t.string 'city'
-    t.string 'state'
-    t.string 'zipcode', limit: 15
-    t.string 'email', null: false
-    t.string 'favorite_beverage'
-    t.string 'password_digest'
-    t.string 'username', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'uid'
-    t.string 'provider'
-    t.index ['email'], name: 'index_users_on_email'
+  create_table "users", force: :cascade do |t|
+    t.string "first_name", null: false
+    t.string "last_name", null: false
+    t.string "address_1"
+    t.string "address_2"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode", limit: 15
+    t.string "email", null: false
+    t.string "favorite_beverage"
+    t.string "password_digest"
+    t.string "username", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "uid"
+    t.string "provider"
+    t.index ["email"], name: "index_users_on_email"
   end
 
-  add_foreign_key 'active_storage_attachments', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'active_storage_variant_records', 'active_storage_blobs', column: 'blob_id'
-  add_foreign_key 'addresses', 'users'
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "addresses", "users"
 end
